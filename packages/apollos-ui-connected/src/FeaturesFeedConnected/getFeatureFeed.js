99eaa4e0e9d80fff7eba8d0587d251dad1d31aa5
Michael Neeley <micneeley14@gmail.com>
2020-10-19 08:51:36 -0400

    made changes

--- packages/apollos-ui-connected/src/FeaturesFeedConnected/getFeatureFeed.js
+++ packages/apollos-ui-connected/src/FeaturesFeedConnected/getFeatureFeed.js
@@ -2,34 +2,17 @@ import ApollosConfig from '@apollosproject/config';
 import gql from 'graphql-tag';
 
 export default gql`
-  query getFeatureFeed($nodeId: ID!) {
-    node(id: $nodeId) {
+  query getFeatureFeed($featureFeedId: ID!) {
+    node(id: $featureFeedId) {
       id
       ... on FeatureFeed {
         features {
           ...FeedFeaturesFragment
         }
       }
-      ... on WeekendContentItem {
-        featureFeed {
-          id
-          features {
-            ...FeaturesFragment
-          }
-        }
-      }
-      ... on ContentSeriesContentItem {
-        featureFeed {
-          id
-          features {
-            ...FeaturesFragment
-          }
-        }
-      }
     }
   }
   ${ApollosConfig.FRAGMENTS.FEED_FEATURES_FRAGMENT}
-  ${ApollosConfig.FRAGMENTS.FEATURES_FRAGMENT}
   ${ApollosConfig.FRAGMENTS.TEXT_FEATURE_FRAGMENT}
   ${ApollosConfig.FRAGMENTS.SCRIPTURE_FEATURE_FRAGMENT}
   ${ApollosConfig.FRAGMENTS.WEBVIEW_FEATURE_FRAGMENT}
